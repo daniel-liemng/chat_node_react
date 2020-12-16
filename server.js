88ea68206273e6e8e41e970ce9a3b235ed6eefdc
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2020-12-16 14:43:12 -0500

    DB Connect

---
+++ server.js
@@ -0,0 +1,23 @@
+const { ApolloServer } = require("apollo-server");
+const { sequelize } = require("./models");
+
+// The GraphQL schema
+const typeDefs = require("./graphql/typeDefs");
+
+// A map of functions which return data for the schema.
+const resolvers = require("./graphql/resolvers");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+
+  // DB Connect
+  sequelize
+    .authenticate()
+    .then(() => console.log("DB Connected"))
+    .catch((err) => console.log("Error", err));
+});
